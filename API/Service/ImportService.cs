ada1313d2e1b3521dcd334a461961b2a34f6dd6a
DESKTOP-J2463JH\fcurt <fcurti1998@outlook.com>
2024-08-26 23:22:10 -0300

    criação da integração com api de correios

---
+++ API/Service/ImportService.cs
@@ -0,0 +1,47 @@
+﻿using Business.Interfaces;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Service
+{
+    public class ImportService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        private readonly IUploadViagemService _viagemService; 
+
+        public ImportService (
+            IUploadViagemService viagemService
+        )
+        {
+            _viagemService = viagemService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(ExecuteImport, null, TimeSpan.FromMinutes(0), TimeSpan.FromMinutes(30));
+            return Task.CompletedTask;
+        }
+        private async void ExecuteImport(object state)
+        {
+            var viagens = new List<string>();
+            foreach (var viagem in viagens)
+            {
+                var a = viagem;
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite,0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
